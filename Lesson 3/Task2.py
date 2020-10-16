4ce5189f0b9885f7f80f68c07770e7c8d0b355ea
Eugene <72600957+ellegeize@users.noreply.github.com>
2020-10-16 16:55:16 +0300

    Add files via upload

---
+++ Lesson 3/Task2.py
@@ -0,0 +1,4 @@
+def my_func(name, surname, byear, city, email, phone):
+    print(name, surname, byear, city, email, phone)
+
+my_func(name= 'eugene', surname='buk', byear=2002, city='msk', email='email', phone='3456')
---
+++ Lesson 3/Task4.py
@@ -0,0 +1,5 @@
+def my_func(x, y):
+    return x ** y
+
+
+print(f'Результат - {my_func(int(input("Введите что возводим ")), int(input("Введите в какую степень возводим ")),)}')
